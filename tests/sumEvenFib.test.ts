b8d5c83a364f20c47abe7ca30bfdbead11763530
Michael James <micrjames921@gmail.com>
2024-08-06 04:11:39 -0700

    separated test suites for each problem; setup code and test suites for each problem

---
+++ tests/sumEvenFib.test.ts
@@ -0,0 +1,12 @@
+import { sumEvenFib } from "../sumEvenFib";
+
+describe("Sum of Even-valued Fibonacci Series", () => {
+   beforeAll(() => {
+   });
+   describe("Values Listed", () => {
+	  test.todo("Should be equal to the given array of numbers.");
+   });
+   describe("Summed", () => {
+	  test.todo("Should be equal to the given number.");
+   });
+});

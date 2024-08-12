de17f23b8aabc58f2937468f40c13b6094613cf8
Michael James <micrjames921@gmail.com>
2024-08-12 11:10:26 -0700

    setup 'largest prime factor' project

---
+++ tests/largestPrimeFactor.test.ts
@@ -0,0 +1,26 @@
+import { largestPrimeFactor } from "../largestPrimeFactor";
+
+describe("The Largest Prime Factor", () => {
+   describe("of 48", () => {
+	  let n: number;
+	  let value: number;
+	  beforeAll(() => {
+		 n = 48;
+		 value = largestPrimeFactor(n);
+	  });
+	  describe("Factored", () => {
+		 test(`Should be equal to the value, ${value}.`);
+	  });
+   });
+   describe("of 600851475143", () => {
+	  let n: number;
+	  let value: number;
+	  beforeAll(() => {
+		 n = 600851475143;
+		 value = largestPrimeFactor(n);
+	  });
+	  describe("Factored", () => {
+		 test(`Should be equal to the value, ${value}.`);
+	  });
+   });
+});

773802b2629155ebe81ea3325192139d5b1bb233
Michael James <micrjames921@gmail.com>
2024-09-12 13:30:11 -0700

    completed code and tests for 'largest palindrome product'

--- largestPalProd.ts
+++ largestPalProd.ts
@@ -8,13 +8,19 @@ A palindromic number reads the same both ways. The largest palindrome made from 
 Find the largest palindrome made from the product of two 3-digit numbers.
 */
 
-export const largestPalProd = (num: number): number => {
-   let nums: number[] = [];
-   for(let i = 10; i < 100; i++)
-      for(let j = 10; j < 100; j++) {
-	    const prod = i * j;
-	  	nums = [...nums, prod];
+export const largestPalProd = (numDigits: number): number => {
+   let res = 0;
+   let prod: number;
+   const start_point = Math.pow(10, numDigits-1);
+   const end_point = Math.pow(10, numDigits);
+   for(let i = start_point; i < end_point; i++)
+      for(let j = start_point; j < end_point; j++) {
+	     prod = i * j;
+		 if(prod > res && isPalindrome(prod)) {
+		    res = prod;
+			console.log(res);
+		 }
 	  }
-   // return isPalindrome(num);
-   return -1;
+
+   return res;
 };
--- tests/largestPalProd.test.ts
+++ tests/largestPalProd.test.ts
@@ -1,22 +1,26 @@
 import { largestPalProd } from "../largestPalProd";
 
 describe("Largest Palindrome Product", () => {
-   beforeAll(() => {
-	 const numbers = [12345, 80008, 44444];
-	 largestPalProd(-1);
-   });
+   let largestPalindromeProduct: number;
+   const numbers = [9009, 906609];
    describe("of two 2-digit numbers", () => {
 	  beforeAll(() => {
+		 largestPalindromeProduct = largestPalProd(2);
 	  });
 	  describe("Multiplied", () => {
-		 test.todo("Should be ...");
+		 test(`Should be ${numbers[0]}`, () => {
+			expect(largestPalindromeProduct).toBe(numbers[0]);
+		 });
 	  });
    });
    describe("of two 3-digit numbers", () => {
 	  beforeAll(() => {
+		 largestPalindromeProduct = largestPalProd(3);
 	  });
 	  describe("Multiplied", () => {
-		 test.todo("Should be ...");
+		 test(`Should be ${numbers[1]}`, () => {
+			expect(largestPalindromeProduct).toBe(numbers[1]);
+		 });
 	  });
    });
 });
